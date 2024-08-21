ce9f3e041e320c71cd5bdfb6f992aa7bf821deba
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2024-08-21 18:59:57 +0700

    fixed deployment v1.5

--- src/js/app.js
+++ src/js/app.js
@@ -6,17 +6,23 @@ const systemTheme  = window.matchMedia("(prefers-color-scheme:dark)").matches;
 
  //check initial theme 
 const checkTheme = () => {
-    //dark mode
-    if (userTheme == "dark" || systemTheme == true)  {
-        document.documentElement.classList.add('dark');
-        localStorage.setItem('theme','dark');
-    } else {
-        localStorage.setItem('theme','light');
-        if ( document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark');
+    if (userTheme == null) {
+        // dark mode
+        if (systemTheme == true)  {
+            document.documentElement.classList.add('dark');
             localStorage.setItem('theme','dark');
+        } else {
+            // light mode
+            localStorage.setItem('theme','light');
+            if ( document.documentElement.classList.contains('dark')) {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('theme','dark');
+            }
         }
     }
+
+
+  
 }
 
 // switch theme
@@ -25,11 +31,13 @@ const switchTheme = () =>{
     if (document.documentElement.classList.contains('dark')) {
         document.documentElement.classList.remove('dark');
         localStorage.setItem('theme','light');
+        return "light"
     }
     // light - dark 
     else {
         document.documentElement.classList.add('dark');
         localStorage.setItem('theme','dark');
+        return "dark"
     } 
 }
   
