8bf1794e4582edf45d42a05a873e20d44c4dc242
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2024-08-21 22:51:15 +0700

    fixed deployment v1.6

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,44 +1,57 @@
-const btnSwitch = document.querySelector('#btn-switch');
-// theme var 
-const userTheme = localStorage.getItem('theme');
+const btnSwitch = document.querySelector("#btn-switch");
+// theme var
+const userTheme = localStorage.getItem("theme");
 // check based on dark theme
-const systemTheme  = window.matchMedia("(prefers-color-scheme:dark)").matches;
+const systemTheme = window.matchMedia("(prefers-color-scheme:dark)").matches;
 
- //check initial theme 
-const checkTheme = () => {
-    if (userTheme == null) {
-        // check systemTheme
-        if (systemTheme)  {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('theme','dark');
-        } else {
-            // light mode
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('theme','dark'); 
-           
-        }
-    }
+// dark mode
+const darkMode = () => {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
 }
+
+// light mode
+const lightMode = () => {
+    if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+      }
+}
+
+//check initial theme
+const checkTheme = () => {
+  if (userTheme == null) {
+    if (systemTheme == true) {
+      darkMode();
+    } else {
+       lightMode();
+    }
+  } else {
+    //dark mode
+    if (userTheme == "dark" || switchTheme == "dark") {
+        darkMode();
+    } else {
+    //   light mode
+      lightMode();
+    }
+  }
+};
 
 // switch theme
-const switchTheme = () =>{
-    // dark - light 
-    if (userTheme == "dark") {
-        document.documentElement.classList.remove('dark');
-        localStorage.setItem('theme','light');
-        return "light"
-    }
-    // light - dark 
-    else {
-        document.documentElement.classList.add('dark');
-        localStorage.setItem('theme','dark');
-        return "dark"
-    } 
-}
-  
-btnSwitch.addEventListener('click',()=>{
-    switchTheme();
-})
+const switchTheme = () => {
+  // dark - light
+  if (document.documentElement.classList.contains("dark")) {
+   lightMode();
+  }
+  // light - dark
+  else {
+    darkMode();
+  }
+};
+
+btnSwitch.addEventListener("click", () => {
+  switchTheme();
+});
 
 // check initial theme
 checkTheme();
